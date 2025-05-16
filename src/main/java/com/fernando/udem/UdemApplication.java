8fb50c98e9d72004d4981f5875ec72ff4c336539
fernandotassi <apropriate3@gmail.com>
2025-05-15 23:03:22 -0300

    project created

---
+++ src/main/java/com/fernando/udem/UdemApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.udem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemApplication 
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(UdemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fernando/udem/UdemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.udem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
